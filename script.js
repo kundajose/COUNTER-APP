04ef4050b64b1f56b321e0fe7023c53494803cc8
IRADUKUNDA Joseph <iradukundajose8@gmail.com>
2022-11-02 16:47:16 +0200

    make changes

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const increase=document.querySelector('#increaments').addEventListener('click',findIncrement);
-// const answer=document.querySelector('h1');
+const answer=document.querySelector('h1');
 const decrease=document.querySelector('#decreaments').addEventListener('click',findDcrement);
+
 let count=0
+answer.innerHTML=count
 
 function findIncrement(){
-    const answer=document.querySelector('h1');
   if(count>=0){
    count++
   }
@@ -12,7 +13,6 @@ function findIncrement(){
 }
 
 function findDcrement(){
-    const answer=document.querySelector('h1');
     if(count>0){
       count--
     }
