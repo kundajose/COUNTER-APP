c78bb21da11dd4a033defca920c1814b432f609a
IRADUKUNDA Joseph <iradukundajose8@gmail.com>
2022-11-02 16:33:01 +0200

    second counter

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const increase=document.querySelector('#increaments').addEventListener('click',f
 // const answer=document.querySelector('h1');
 const decrease=document.querySelector('#decreaments').addEventListener('click',findDcrement);
 let count=0
-// answer.innerHTML=count
+
 function findIncrement(){
     const answer=document.querySelector('h1');
   if(count>=0){
